1b0259be272709b64ae65ef42f4844a0a2b01244
dssekh <157597693+dssekh@users.noreply.github.com>
2024-01-24 17:10:03 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductListing from './ProductListing';
+import Cart from './Cart';
+
+function App() {
+  return (
+    <div>
+      <ProductListing />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
